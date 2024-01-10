d460da88c265f4eb7e40d89974e7c5267c1f7dce
Rommy Ramos <119701688+rommyrr92@users.noreply.github.com>
2024-01-09 22:13:24 -0500

    Update opinion.php

--- secciones/opinion.php
+++ secciones/opinion.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $opinion = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Desafíos Educativos: La Urgencia de una Reforma Integral",
+"autor" => "Ana García",
+"resumen" => "Ana García ofrece una perspectiva crítica sobre los desafíos actuales en el sistema educativo, argumentando la necesidad imperante de una reforma integral. ",
 ];
 ?>
