00b6802db28d11cb91f1788f822dd61409b59710
Rommy Ramos <119701688+rommyrr92@users.noreply.github.com>
2024-01-09 22:22:00 -0500

    Update deportes.php

--- secciones/deportes.php
+++ secciones/deportes.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $deportes = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Deporte y Sociedad: Más Allá de la Competencia",
+"autor" => "Laura Fernández",
+"resumen" => "En su análisis, Laura Fernández examina el papel integral que desempeña el deporte en la sociedad contemporánea, y cómo va más allá de la competencia pura. Explora cómo los eventos deportivos se han convertido en plataformas para la expresión cultural, la unidad nacional y la promoción de valores fundamentales. ",
 ];
 ?>
