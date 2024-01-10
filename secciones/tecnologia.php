ea487266da917dfca719ae1a3ebfb6bfb49d1fc9
Rommy Ramos <119701688+rommyrr92@users.noreply.github.com>
2024-01-09 22:14:42 -0500

    Update tecnologia.php

--- secciones/tecnologia.php
+++ secciones/tecnologia.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $tecnologia = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Inteligencia Artificial en la Medicina: Avances Prometedores y Desafíos Éticos",
+"autor" => "Carlos Vargas",
+"resumen" => "El artículo explora cómo la IA ha revolucionado el diagnóstico médico, el descubrimiento de fármacos y la personalización de tratamientos, mejorando significativamente la eficacia y la eficiencia en la atención médica.",
 ];
 ?>
