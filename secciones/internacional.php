6da4f0ff360cb92147e0af2b21e475c07eec7ad1
Rommy Ramos <119701688+rommyrr92@users.noreply.github.com>
2024-01-09 22:07:05 -0500

    Update internacional.php

--- secciones/internacional.php
+++ secciones/internacional.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $internacional = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Revolución Cuántica en la Computación: Desarrollos Innovadores y Desafíos Globales",
+"autor" => "Dr. Elena Ramírez",
+"resumen" => "En este artículo, el Dr. Elena Ramírez, una destacada científica en el campo de la computación cuántica, examina los avances más recientes y emocionantes en esta revolucionaria tecnología. ",
 ];
 ?>
