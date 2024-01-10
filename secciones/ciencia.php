e531880b115afeb2a479074acdce19361ff6185e
Rommy Ramos <119701688+rommyrr92@users.noreply.github.com>
2024-01-09 22:16:49 -0500

    Update ciencia.php

--- secciones/ciencia.php
+++ secciones/ciencia.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $ciencia = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Explorando los Misterios del Cosmos: Avances Recientes en la Investigación Cósmica",
+"autor" => "Dra. Laura Gómez",
+"resumen" => "Desde la detección de ondas gravitacionales hasta el estudio de exoplanetas habitables, el artículo aborda descubrimientos clave que han surgido de la colaboración internacional entre astrónomos y científicos espaciales. ",
 ];
 ?>
