234f0d6bdd739896dbc4e5a8cd799721132f1285
Rommy Ramos <119701688+rommyrr92@users.noreply.github.com>
2024-01-09 22:19:03 -0500

    Update gente.php

--- secciones/gente.php
+++ secciones/gente.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $gente = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Empoderamiento Juvenil: La Voz de una Generación en Transformación",
+"autor" => "Alejandro Torres",
+"resumen" => "Desde movimientos activistas hasta iniciativas comunitarias, el autor destaca cómo la juventud de hoy está desempeñando un papel crucial en la definición de cambios sociales y políticos. Torres entrevista a jóvenes líderes de diversos ámbitos para ofrecer una visión íntima de sus experiencias, desafíos y aspiraciones.",
 ];
 ?>
