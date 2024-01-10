2b8e3255ac51a7209fc897c12ef06da735602360
Rommy Ramos <119701688+rommyrr92@users.noreply.github.com>
2024-01-09 22:09:13 -0500

    Update nacional.php

--- secciones/nacional.php
+++ secciones/nacional.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $nacional = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Innovación Sostenible en la Agricultura: Un Enfoque Nacional para la Seguridad Alimentaria",
+"autor" => "Dra. María López",
+"resumen" => "La Dra. María López, experta en agricultura sostenible, presenta un análisis exhaustivo sobre las estrategias nacionales para impulsar la innovación en el sector agrícola. ",
 ];
 ?>
