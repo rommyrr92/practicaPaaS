1711a9aefb4786bb3625db10ee63ad4f50d41080
Rommy Ramos <119701688+rommyrr92@users.noreply.github.com>
2024-01-09 22:17:43 -0500

    Update cultura.php

--- secciones/cultura.php
+++ secciones/cultura.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $cultura = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Globalización Cultural: Desafíos y Oportunidades en un Mundo Conectado",
+"autor" => "Marta Sánchez",
+"resumen" => "Desde la difusión de tendencias gastronómicas hasta la influencia de las plataformas de streaming en la música y el cine, la autora explora cómo la interconexión global está dando forma a nuestras identidades culturales. ",
 ];
 ?>
