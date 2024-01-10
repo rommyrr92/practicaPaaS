9f8e638fcc392949504bfff075427f0e987eafd5
Rommy Ramos <119701688+rommyrr92@users.noreply.github.com>
2024-01-09 22:12:10 -0500

    Update economia.php

--- secciones/economia.php
+++ secciones/economia.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $economia = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Transformación Digital en Empresas Emergentes: Perspectivas para el Crecimiento Económico",
+"autor" => "Juan Martínez",
+"resumen" => "En este artículo, Juan Martínez explora el impacto de la transformación digital en las empresas emergentes y su contribución al crecimiento económico. ",
 ];
 ?>
