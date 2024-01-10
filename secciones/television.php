e89966d116e9f099d814d531ee4c0ae6d34be40a
Rommy Ramos <119701688+rommyrr92@users.noreply.github.com>
2024-01-09 22:23:47 -0500

    Update television.php

--- secciones/television.php
+++ secciones/television.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $television = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Evolución Televisiva: La Transformación del Entretenimiento en la Era Digital",
+"autor" => "Roberto Martínez",
+"resumen" => "En su análisis, destaca cómo este cambio ha alterado no solo la forma en que consumimos contenido, sino también la naturaleza misma de la narrativa televisiva. ",
 ];
 ?>
